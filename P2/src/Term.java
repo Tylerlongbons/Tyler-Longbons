cd04ac171e6795d5368e3f94a7fff8e196972d22
Tylerlongbons <tjlongbons@hotmail.com>
2014-09-28 23:09:46 -0600

    tylers updated code as of 9/28

--- P2/src/Term.java
+++ P2/src/Term.java
@@ -1,52 +1,47 @@
 import java.util.LinkedList;
-public class Term {
+public class Term implements Comparable<Term>{
 
 	String name;
 	int docFrequency;
 	int totalFrequency;
 	LinkedList<Occurrence> docsList = new LinkedList<Occurrence>();
-	
+
 	public Term(String name){
 		this.name = name.toLowerCase();
 		docFrequency = 0;
+		totalFrequency = 0;
 	}
 	public void incFrequency(String document){
 		totalFrequency++;
-		int i = 0;
-		boolean found = false;
-			if(docsList.size() > 0){
-				while(docsList.get(i) != null){
-					
-					if(docsList.get(i).equals(document)){
-						found = true;
-						break;
-					}
-					i++;
-				}
-				if(!found){
-					Occurrence occur = new Occurrence(document);
-					docsList.addFirst(occur);
-				}else{
-					docsList.get(i).incFrequency();
-					docFrequency++;
-				}
-			}else{
-				Occurrence occur = new Occurrence(document);
-				docsList.addFirst(occur);
+
+		boolean isNew = true;
+
+		for(Occurrence o: docsList){
+			if(o.docName.equals(document)){
+				o.termFrequency++;
+				isNew = false;
+				break;
 			}
-			
+		}
+		if(isNew){
+			Occurrence newOcc = new Occurrence(document);
+			docsList.add(newOcc);
+		}
+		docFrequency = docsList.size();
+
+
 	}
 	public int compareTo(Term o){
-		if(this.totalFrequency < o.totalFrequency){
+		if(this.totalFrequency > o.totalFrequency){
 			return -1;
 		}
-		else if(this.totalFrequency > o.totalFrequency){
+		else if(this.totalFrequency < o.totalFrequency){
 			return 1;
 		}else{
 			return 0;
 		}
 	}
-	
+
 	public int compareWords(Term o){
 		if(this.name.compareTo(o.name) == 0){
 			return 0;
@@ -56,6 +51,6 @@ public class Term {
 			return 1;
 		}
 	}
-	
-	
+
+
 }
--- P2/src/WebPages.java
+++ P2/src/WebPages.java
@@ -16,120 +16,17 @@ public class WebPages {
 	}
 	public void addPage(String filename){
 
-		ReadDoc docReader = new ReadDoc(filename);
-
-		int counter = 0;
-		int wordsSize = docReader.words.size();
-		while(counter < wordsSize){
-			String word = docReader.words.get(counter);
-			Term newTerm = new Term(word);
-			newTerm.incFrequency(filename);
-			termIndex.add(newTerm);
-			counter++;
-		}
+		ReadDoc docReader = new ReadDoc(termIndex);
+		docReader.readFile(filename);
 	}
-	public static void printTerms(){
-		System.out.println("WORDS");
-		for(Term t: termIndex){
-			System.out.println(t.name);
-		}
-	}
-	//Prichard and Carrano pgs (529-531)
-	private static void mergeSort(ArrayList<Term> theArray, int first, int last, sortType type){
-
-		if(first < last){
-			int mid = (first + last) / 2;
-			mergeSort(theArray,first, mid, type);
-			mergeSort(theArray, mid + 1, last, type);
-
-			merge(theArray, first, mid, last, type);
-		}
-
-
-	}
-	//Prichard and Carrano pgs (529-531)
-	private static void merge(ArrayList<Term> theArray,int first, int mid, int last, sortType type){
-		int first1 = first;
-		int last1 = mid;
-		int first2 = mid + 1;
-		int last2 = last;
-		int index = first1;
-		int counter = 0; //this needs to be in output
-		ArrayList<Term> tempArray = new ArrayList<>();
-
-		while((first <= last1) && (first2 <= last2)){
-			if(type == sortType.totalFrequency){
-				
-					if(theArray.get(first1).compareTo(theArray.get(first2)) <= 0){ 
-						tempArray.add(index, theArray.get(first1));
-						first1++;
-						counter++;  //this needs to be in output
-					}else{
-						tempArray.add(index, theArray.get(first2));
-						first2++;
-					}
-					index++;
-				}else{
-					
-					if(theArray.get(first1).compareWords(theArray.get(first2)) <= 0){ 
-						tempArray.add(index, theArray.get(first1));
-						first1++;
-						counter++;  //this needs to be in output
-					}else{
-						tempArray.add(index, theArray.get(first2));
-						first2++;
-					}
-					index++;
-				}
-		}
-
-		while(first1 <= last1){
-			tempArray.set(index, theArray.get(first));
-			first1++;
-			index++;
-		}
-		while(first1 <= last2){
-			tempArray.set(index, theArray.get(first2));
-			first2++;
-			index++;
-		}
-		for(index = first; index <= last; ++index){
-			theArray.set(index, tempArray.get(index));
-		}
-		mergeSortCount += counter;
-	} //end merge
-
-
-	public void pruneStopWords(int n){
-		//TODO
-
-		mergeSort(termIndex, 0, termIndex.size(),sortType.totalFrequency);
-		for(int i = 0; i < n; i++){
-			termIndex.remove(i);
-		}
-		mergeSort(termIndex, 0, termIndex.size(),sortType.words);
-	}
-	//	public String[] whichPages(String word){
-	//		ArrayList<String> s = new ArrayList<String>();
-	//		for(Term t: termIndex){
-	//			if(t.name.equals(word)){
-	//				for(Occurrence o: t.occurrences){
-	//					s.add(o.docName);
-	//				}
-	//				return (String[]) s.toArray();
-	//			}
-	//		}
-	//		return null;
-	//	}
-
 	public class ReadDoc {
 
-		public ArrayList<String> words = new ArrayList<String>();
+		public ArrayList<Term> words;
 		private ArrayList<String> tag = new ArrayList<String>();
 		private boolean inTag =false;
 
-		public ReadDoc(String filename){
-			readFile(filename);
+		public ReadDoc(ArrayList<Term> terms){
+			this.words = terms;
 		}
 
 		public void readFile(String filename) {
@@ -146,16 +43,30 @@ public class WebPages {
 						tagWord(w);
 						for (String s : tag) {
 							s = punct(s);
-							if (binarySearch(s, 0, words.size() - 1)>-1
-									&& s.length() > 0) {
-								words.add(binarySearch(s,0,words.size()-1),s);
-							}
+							int srchRslt = binarySearch(s, 0, words.size() - 1);
+							if(srchRslt >-1 && s.length() > 0){
+								Term t = new Term(s);
+								t.incFrequency(filename);;
+								words.add(srchRslt, t);
+							}else
+								if(srchRslt == -1){
+									Term t = getTerm(s);
+									t.incFrequency(filename);;
+								}
 						}
 						tag = new ArrayList<String>();
 					} else if(!inTag){
 						w = punct(w);
-						if (binarySearch(w, 0, words.size() - 1)>-1 && w.length() > 0) {
-							words.add(binarySearch(w,0,words.size()-1),w);
+						int srchRslt = binarySearch(w, 0, words.size() - 1);
+						if(srchRslt >-1 && w.length() > 0) {
+							Term t = new Term(w);
+							t.incFrequency(filename);;
+							words.add(srchRslt, t);
+						}else{
+							if(srchRslt == -1){
+							Term t = getTerm(w);
+							t.incFrequency(filename);
+							}
 						}
 					}
 				}
@@ -164,6 +75,16 @@ public class WebPages {
 				// TODO How do they want errors
 				e.printStackTrace();
 			}
+		}
+		private Term getTerm(String s){
+			Term term = null;
+			for(Term t: words){
+				if(t.name.equals(s)){
+					term = t;
+					break;
+				}
+			}
+			return term;
 		}
 
 		private String punct(String w) {
@@ -203,36 +124,161 @@ public class WebPages {
 				result= first;
 			else {
 				int mid = (first + last) / 2;
-				if (s.equals(words.get(mid)))
+				if (s.equals(words.get(mid).name))
 					result= -1;//implies found
-				else if (s.compareTo(words.get(mid)) < 0)
+				else if (s.compareTo(words.get(mid).name) < 0)
 					result= binarySearch(s, first, mid - 1);
-				else if (s.compareTo(words.get(mid)) > 0)
+				else if (s.compareTo(words.get(mid).name) > 0)
 					result= binarySearch(s, mid + 1, last);
 			}
 			return result;
 		}
 	}  //end readDoc class
+	
+	public static void printTerms(){
+		  System.out.println("WORDS");
+		  for(Term t: termIndex){
+		   String msg;
+		   if (t.name.length() <= 2) {
+		    msg = "term:"+t.name+"\t\tdoc_freq:"+t.docFrequency+"\ttotal_freq:"+t.totalFrequency;
+		   } else {
+		    msg = "term:"+t.name+"\tdoc_freq:"+t.docFrequency+"\ttotal_freq:"+t.totalFrequency;
+		   }
+		    
+		   System.out.println(msg);
+		  }
+		
+		//		System.out.println("WORDS");
+//		for(Term t: termIndex){
+//			System.out.println(t.name);
+//		}
+	}
+	//Prichard and Carrano pgs (529-531)
+	private static void mergeSort(ArrayList<Term> theArray,ArrayList<Term> tempArray, int first, int last, sortType type){
+		
+		if(first < last){
+			int mid = (first + last) / 2;
+			mergeSort(theArray,tempArray, first, mid, type);
+			mergeSort(theArray,tempArray, mid + 1, last, type);
+
+			merge(theArray,tempArray, first, mid, last, type);
+		}
+		
+
+	}
+	//Prichard and Carrano pgs (529-531)
+	private static void merge(ArrayList<Term> theArray,ArrayList<Term> tempArray,int first, int mid, int last, sortType type){
+		int first1 = first;
+		int last1 = mid;
+		int first2 = mid + 1;
+		int last2 = last;
+		int index = first1;
+		int counter = 0; //this needs to be in output
+
+		while((first1 <= last1) && (first2 <= last2)){
+			Term f1 = theArray.get(first1);
+			Term f2 = theArray.get(first2);
+			if(type == sortType.totalFrequency){
+				int cmp = f1.compareTo(f2);
+				if(cmp <= 0){ 
+					tempArray.add(index, f1);
+					first1++;
+					counter++;  //this needs to be in output
+				}else{
+					tempArray.set(index, f2);
+					first2++;
+				}
+				index++;
+			}else{
+				int cmp = f1.compareWords(f2);
+				if(cmp <= 0){ 
+					tempArray.add(index, f1);
+					first1++;
+					counter++;  //this needs to be in output
+				}else{
+					tempArray.set(index, f2);
+					first2++;
+				}
+				index++;
+			}
+			
+		}
+
+		while(first1 <= last1){
+			Term f1 = theArray.get(first1);
+			tempArray.set(index, f1);
+			first1++;
+			index++;
+		}
+		while(first2 <= last2){
+			Term f2 = theArray.get(first2);
+			tempArray.add(index, f2);
+			first2++;
+			index++;
+		}
+	
+			for(index = first; index <= last; ++index){
+				theArray.set(index, tempArray.get(index));
+			}
+		
+		mergeSortCount += counter;
+	} //end merge
 
 
-	public static void main(String[] args){
+	public void pruneStopWords(int n){
+		//TODO
+		ArrayList<Term> tempArray = new ArrayList<Term>();
+		mergeSortCount = 0;
+		mergeSort(termIndex,tempArray, 0, termIndex.size() - 1,sortType.totalFrequency);
+		System.out.println("Copies: "+ mergeSortCount);
+		for(int i = 0; i < n; i++){
+			termIndex.remove(0);
+		}
+		mergeSortCount = 0;
+		mergeSort(termIndex,tempArray, 0, termIndex.size() - 1,sortType.words);
+		System.out.println("Copies: " + mergeSortCount);
+	}
+	//	public String[] whichPages(String word){
+	//		ArrayList<String> s = new ArrayList<String>();
+	//		for(Term t: termIndex){
+	//			if(t.name.equals(word)){
+	//				for(Occurrence o: t.occurrences){
+	//					s.add(o.docName);
+	//				}
+	//				return (String[]) s.toArray();
+	//			}
+	//		}
+	//		return null;
+	//	}
+
+	
+		public static void main(String[] args){
 		WebPages page = new WebPages();
 		ArrayList<String> list = new ArrayList<>();
+		String inputfile = "input2.txt";
+		//String inputFile = args[0];
 		Scanner scanFile = null;
 		try {
-			scanFile = new Scanner(new File(args[0]));
+			scanFile = new Scanner(new File(inputfile));
 			while (scanFile.hasNextLine()){
 				list.add(scanFile.nextLine());
 			}
 			int i = 0;
 
 			while(!list.get(i).equals("*EOFs*")){
-				page.addPage(list.get(i));
+				String pageName = list.get(i);
+				page.addPage(pageName);
 				i++;
 			}
 			printTerms();
-			int stopWords = Integer.parseInt(list.get(i + 1));
+			
+			i++;
+			int stopWords = Integer.parseInt(list.get(i));
 			page.pruneStopWords(stopWords);
+			
+			printTerms();
+			
+			scanFile.close();
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
